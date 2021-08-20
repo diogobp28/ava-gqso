c81199fab606c6a20cec0ef2669aae9c4e8d5282
diogobp28 <dbp1@aluno.ifal.edu.br>
2021-08-20 19:09:18 +0000

    Funçãosoma da calculadora API adicionada

---
+++ funcao_soma.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+	"strings"
+)
+
+func soma(x, y) float64 {
+	return x + y
+}
+
+func somaHandler(
+	
+	teste1 http.ResponseWriter,
+	teste2 *http.Request) {
+	c := strings.Split(teste2.URL.Path, "/")
+	num1, err := strconv.ParseFloat(c [2], 64)
+	
+	if err != nil {
+		teste1.WriteHeader(400)
+		
+		return
+	}
+
+	num2, err := strconv.ParseFloat(c[3], 64)
+	
+	if err != nil {
+		teste1.WriteHeader(400)
+		
+		return
+	}
+
+	result := soma(num1, num2)
+	fmt.Fprintf(teste1, "%f", result)
+	
+}
